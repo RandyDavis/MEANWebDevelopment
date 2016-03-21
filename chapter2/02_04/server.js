c6a43bb9231e261356a764fd08e5acea4fe228df
Randy Davis <randy_davis@outlook.com>
2016-03-21 01:02:06 -0500

    add remaining connect module examples

---
+++ chapter2/02_04/server.js
@@ -0,0 +1,12 @@
+/**
+ * Created by randy on 3/20/16.
+ */
+'use strict';
+
+var connect = require('connect');
+
+var app = connect();
+
+app.listen(3000);
+
+console.log('Server running at http://localhost:3000/');
---
+++ chapter2/02_05/server.js
@@ -0,0 +1,17 @@
+/**
+ * Created by randy on 3/21/16.
+ */
+'use strict';
+
+var connect = require('connect');
+var app = connect();
+
+var helloWorld = function (req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+};
+
+app.use(helloWorld);
+
+app.listen(3000);
+console.log('Server running at http://localhost:3000/');
