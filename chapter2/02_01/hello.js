ffa0bc1d2ee2b1feb1705ae38b68ed8b09b57971
Randy Davis <randy_davis@outlook.com>
2016-03-20 23:27:51 -0500

    add 02_01 lesson - CommonJS modules example

---
+++ chapter2/02_01/hello.js
@@ -0,0 +1,10 @@
+/**
+ * Created by randy on 3/20/16.
+ */
+'use strict';
+
+var message = 'Hello';
+
+exports.sayHello = function () {
+    console.log(message);
+}
---
+++ chapter2/02_01/server.js
@@ -0,0 +1,8 @@
+/**
+ * Created by randy on 3/20/16.
+ */
+'use strict';
+
+var hello = require('./hello');
+
+hello.sayHello();
